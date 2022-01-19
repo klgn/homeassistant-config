0d345325d354e49143e600a90a7e14d0e26117be
Kristian L. G. Nielsen <kristian@lgnielsen.dk>
2022-01-19 18:13:05 +0100

    Remove stuff

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,10 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-8113c4f2.js')")();
+  new Function("import('/hacsfiles/frontend/main-25487c89.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-8113c4f2.js';
+  el.src = '/hacsfiles/frontend/main-25487c89.js';
+  el.type = 'module';
   document.body.appendChild(el);
 }
   
