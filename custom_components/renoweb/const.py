68db465657d21cd8519c9558e7878143a8fb24bd
Kristian L. G. Nielsen <kristian@lgnielsen.dk>
2023-02-08 18:12:19 +0100

    Remove more custom components from github

--- custom_components/renoweb/const.py
+++ custom_components/renoweb/const.py
@@ -3,13 +3,16 @@
 API_KEY_MUNICIPALITIES = "DDDD4A1D-DDD1-4436-DDDD-3F374DD683A1"
 API_KEY = "346B43B0-D1F0-4AFC-9EE8-C4AD1BFDC218"
 
+ATTR_DATA_VALID = "data_valid"
 ATTR_DESCRIPTION = "description"
+ATTR_ICON_COLOR = "icon_color"
 ATTR_NEXT_PICKUP_TEXT = "next_pickup"
 ATTR_NEXT_PICKUP_DATE = "next_pickup_date"
 ATTR_SCHEDULE = "schedule"
 ATTR_REFRESH_TIME = "refresh_time"
 ATTR_FORMATTED_STATE_DK = "formatted_state_dk"
 ATTR_SHORT_STATE_DK = "short_state_dk"
+ATTR_STATE_TEXT = "state_text"
 
 CONF_ADDRESS = "address"
 CONF_ADDRESS_ID = "address_id"
@@ -26,4 +29,20 @@ DEFAULT_API_VERSION = "1.3"
 DEFAULT_SCAN_INTERVAL = 6
 DOMAIN = "renoweb"
 
-INTEGRATION_PLATFORMS = ["sensor"]
+INTEGRATION_PLATFORMS = ["sensor", "binary_sensor"]
+
+
+ICON_LIST = {
+    "Restaffald-Madaffald": "mdi:trash-can",
+    "Dagrenovation": "mdi:trash-can",
+    "Metal-Glas": "mdi:glass-fragile",
+    "PAPPI": "mdi:recycle",
+    "Farligt affald": "mdi:biohazard",
+    "Tekstiler": "mdi:hanger",
+    "Jern": "mdi:bucket",
+    "Papir": "mdi:file",
+    "Pap": "mdi:note",
+    "Plast Metal": "mdi:trash-can-outline",
+    "Storskrald": "mdi:table-furniture",
+    "Haveaffald": "mdi:leaf",
+}
